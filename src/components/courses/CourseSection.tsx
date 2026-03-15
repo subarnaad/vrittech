315cd3affd44d48c47c753c617896d014126ec24
subarnaad <133129998+subarnaad@users.noreply.github.com>
2026-03-15 20:14:17 +0545

    css fix

--- src/components/courses/CourseSection.tsx
+++ src/components/courses/CourseSection.tsx
@@ -1,11 +1,10 @@
 "use client"
-import Link from "next/link"
 import { useState } from "react"
 import CourseCard from "./CourseCard"
 import { courseData, courseImages } from "@/src/data/courseData"
-import { ArrowLeft } from "lucide-react"
 
 export default function CourseSection() {
+
 
     const [active, setActive] = useState(0)
 
@@ -16,8 +15,6 @@ export default function CourseSection() {
     return (
 
         <div className="flex gap-6 mx-20 w-auto h-150 mt-10">
-
-
             {courseData.map((card) => (
                 <CourseCard
                     key={card.id}
